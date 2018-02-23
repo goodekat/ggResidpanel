4b352725835487c8e76f401df9c719f1d2a1c3e9
goodekat <kgoode@iastate.edu>
2018-02-23 11:27:43 -0600

    added code for residlev plot

---
+++ R/residlev_plot.R
@@ -0,0 +1,20 @@
+
+
+library(MASS)
+
+residlev_plot <- function(model){
+
+  # Create a data frame with the leverage values and standardized residuals
+  model_values <- data.frame(leverage = hatvalues(model),
+                             std_res = stdres(model))
+
+  # Create the leverage plot
+  ggplot(model_values, aes(x = leverage, y = std_res)) +
+    geom_point() +
+    geom_abline(slope = 0, intercept = 0) +
+    labs(x = "Leverage", y = "Standardized Residuals", title = "Residuals vs Leverage") +
+    expand_limits(x = 0) +
+    theme_bw(base_size = 10)
+}
+
+lev_plot(model1)
