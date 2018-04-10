dda22cb400ba6fc9a848d8601c3aafc4b9907564
reykp <kathleenrey89@gmail.com>
2018-04-10 12:22:28 -0500

    Added cook's d function to ggResidpanel

---
+++ R/resid_cookd.R
@@ -0,0 +1,44 @@
+#' Cook's D Plot.
+#'
+#' Creates a Cook's D plot with the Cook's D values versus the observation number.
+#'
+#' @param model Model fit using either lm or glm.
+#' @export
+#' @return A plot of the Cook's D values versus observation numbers from the \code{model}.
+#' The Horizontal line represents a cut-off to identify highly influential points. The horizontal line is
+#' placed at 4/n where n is the number of data points used in the \code{model}.
+#' @examples
+#' model <- lm(Volume ~ Girth, data = trees)
+#' resid_cookd(model)
+
+
+resid_cookd <- function(model){
+
+  # Return an error if a model is not entered in the function
+  if(typeof(model) == "double")
+    stop("The updated version of ggResidpanel requires a model to be input to the functions.
+         Accepted models currently are lm and glm.")
+
+  if(!(class(model)[1] %in% c("lm", "glm")))
+    stop("The 'cooks.distance' function used to create this plot requires a lm or glm model.")
+
+  # Create a data frame with the cook's d values and the observation numbers
+  model_values <- data.frame(cooksd = cooks.distance(model),
+                             obs = 1:length(resid(model)))
+
+  #Alternative
+  #k <- length(model$coefficients)-1
+  #cutoff <- qf(.2, k+1, length(model$residuals)-k-1)
+  #SAS
+  cutoff <- 4/length(resid(model))
+
+  ggplot(model_values, aes(x = obs, y = cooksd)) + geom_point() +
+    geom_segment(aes(xend=obs, yend=0), color="blue")+
+    labs(x = "Observation", y = "COOK's D", title = "COOK's D") +
+    geom_hline(yintercept=cutoff, colour="blue", linetype=5)+
+    geom_hline(yintercept=0, colour="black")+    theme_bw() +
+    theme(plot.title = element_text(size = 12, face = "bold"),
+          axis.title = element_text(size = 10))
+
+
+}
