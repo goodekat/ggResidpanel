61667afe14e613fb819e48814c307309c5f05af5
goodekat <kgoode@iastate.edu>
2018-04-11 16:04:00 -0500

    worked on leverage plot

--- R/resid_lev.R
+++ R/resid_lev.R
@@ -19,6 +19,15 @@ resid_lev <- function(model){
   model_values <- data.frame(leverage = hatvalues(model),
                              std_res = stdres(model))
 
+  cook.levels = c(0.5, 1)
+  hii <- (infl <- influence(model, do.coef = FALSE))$hat
+  r.hat <- range(hii, na.rm = TRUE)
+  p <- model$rank
+  usr <- par("usr")
+  hh <- seq.int(min(r.hat[1L], r.hat[2L]/100), usr[2L], length.out = 101)
+  cl.h1 <- sqrt(cook.levels[1] * p * (1 - hh)/hh)
+  cl.h2 <- sqrt(cook.levels[2] * p * (1 - hh)/hh)
+
   # Create the leverage plot
   ggplot(model_values, aes(x = leverage, y = std_res)) +
     geom_point() +
@@ -27,7 +36,13 @@ resid_lev <- function(model){
     geom_smooth(color = "red", se = FALSE, method = 'loess', size = 0.5) +
     geom_hline(yintercept = 0, linetype = "dashed") +
     geom_vline(xintercept = 0, linetype = "dashed") +
-    theme_classic() +
+    geom_line(d = data.frame(x = hh, y = cl.h1), aes(x, y), linetype = "dashed", color = "red") +
+    geom_line(d = data.frame(x = hh, y = -cl.h1), aes(x, y), linetype = "dashed", color = "red") +
+    geom_line(d = data.frame(x = hh, y = cl.h2), aes(x, y), linetype = "dashed", color = "red") +
+    geom_line(d = data.frame(x = hh, y = -cl.h2), aes(x, y), linetype = "dashed", color = "red") +
+    scale_x_continuous(limits = c(0, max(model_values$leverage, na.rm = TRUE))) +
+    scale_y_continuous(limits = extendrange(range(model_values$std_res, na.rm = TRUE), f = 0.08)) +
+    theme_bw() +
     theme(plot.title = element_text(size = 12, face = "bold"),
           axis.title = element_text(size = 10))
 }
