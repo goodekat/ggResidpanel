c27bc7c04adf1bc73e346026acc41f744ee42395
reykp <kathleenrey89@gmail.com>
2018-04-19 17:24:00 -0500

    Added label function to histogram

--- R/resid_hist.R
+++ R/resid_hist.R
@@ -12,22 +12,8 @@
 #' model <- lm(Volume ~ Girth, data = trees)
 #' resid_hist(model)
 
-resid_hist <- function(model, bins = NA, type=NA){
-=======
-# Histogram of Residuals.
-#
-# Creates a histogram of the residuals from a model.
-#
-# @param model Model fit using either lm, glm, lmer, or glmer.
-# @return A histogram of the residuals from the \code{model} with a normal
-# density curve overlaid with mean equal to the mean of the residuals and
-# standard deviation equal to the standard deviation of the residuals.
-# @examples
-# model <- lm(Volume ~ Girth, data = trees)
-# resid_hist(model)
 
-resid_hist <- function(model, bins = NA, theme, axis.text.size, title.text.size, title){
->>>>>>> a9a048f1974c38ade543c5f5ec06989d615834b5
+resid_hist <- function(model, bins = NA, theme=NA, axis.text.size=12, title.text.size=12, title=TRUE){
 
   #If bins=NA, use default
   if(is.na(bins)){
@@ -36,7 +22,7 @@ resid_hist <- function(model, bins = NA, theme, axis.text.size, title.text.size,
   # Return an error if a model is not entered in the function
   if(typeof(model) == "double")
     stop("The updated version of ggResidpanel requires a model to be input to the functions.
-         Accepted models currently are lm and glm.")
+         Accepted models currently are lm, glm, lmer, and glmer.")
 
   #call function to return appropriate residual label
   r_label <- resid_label(type, model)
@@ -47,7 +33,7 @@ resid_hist <- function(model, bins = NA, theme, axis.text.size, title.text.size,
     model_values <- data.frame(resid = resid(model), type=type)
   }
 
-
+  Default_Title <- paste("Boxplot of", r_label)
   #Step to make sure are not cutting out any huge outliers
   if (min(model_values$resid) < -4*sd(model_values$resid)){
     min_x <- NA
@@ -70,13 +56,9 @@ resid_hist <- function(model, bins = NA, theme, axis.text.size, title.text.size,
       stat_function(fun = dnorm, color = "blue",
                     args = list(mean = 0,
                                 sd = sd(model_values$resid))) +
-<<<<<<< HEAD
-      labs(x = r_label, y = "Density", title = "Histogram of Residuals") +
+      labs(x = r_label, y = "Density") +
       theme(plot.title = element_text(size = 12, face = "bold"),
             axis.title = element_text(size = 10))
-=======
-      labs(x = "Residuals", y = "Density", title = "Histogram of Residuals")
->>>>>>> a9a048f1974c38ade543c5f5ec06989d615834b5
   }else{
   # Create the histogram of residuals
   plot <- ggplot(model_values, aes(x = resid)) +
@@ -85,17 +67,16 @@ resid_hist <- function(model, bins = NA, theme, axis.text.size, title.text.size,
     stat_function(fun = dnorm, color = "blue",
                   args = list(mean = 0,
                               sd = sd(model_values$resid))) +
-<<<<<<< HEAD
-    labs(x = r_label, y = "Density", title = "Histogram of Residuals") +
+    labs(x = r_label, y = "Density") +
       xlim(c(min_x, max_x))+
     theme(plot.title = element_text(size = 12, face = "bold"),
-          axis.title = element_text(size = 10))
-=======
-    labs(x = "Residuals", y = "Density") +
-      xlim(c(min_x, max_x))
+          axis.title = element_text(size = 10))+
+    labs(x = r_label, y = "Density")
   }
 
+
   # Add theme to plot
+  if(!is.na(theme)){
   if (theme == "bw"){
     plot <- plot + theme_bw()
   } else if (theme == "classic"){
@@ -103,16 +84,15 @@ resid_hist <- function(model, bins = NA, theme, axis.text.size, title.text.size,
   } else if (theme == "gray" | theme == "grey"){
     plot <- plot + theme_grey()
   }
-
+}
   # Set text size of title and axis lables, determine whether to include a title, and return plot
   if(title == TRUE){
     plot +
-      labs(title = "Histogram of Residuals") +
+      labs(title =  Default_Title) +
       theme(plot.title = element_text(size = title.text.size, face = "bold"),
             axis.title = element_text(size = axis.text.size))
   } else if (title == FALSE){
     plot + theme(axis.title = element_text(size = axis.text.size))
->>>>>>> a9a048f1974c38ade543c5f5ec06989d615834b5
   }
 
 }
