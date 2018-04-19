549008f2cc36443720e7d89e72bd440c646222c7
reykp <kathleenrey89@gmail.com>
2018-04-19 17:15:08 -0500

    Updated theme option

--- R/resid_cookd.R
+++ R/resid_cookd.R
@@ -10,12 +10,12 @@
 # model <- lm(Volume ~ Girth, data = trees)
 # resid_cookd(model)
 
-resid_cookd <- function(model, theme, axis.text.size, title.text.size, title){
+resid_cookd <- function(model, theme=NA, axis.text.size=12, title.text.size=12, title=TRUE){
 
   # Return an error if a model is not entered in the function
   if(typeof(model) == "double")
     stop("The updated version of ggResidpanel requires a model to be input to the functions.
-         Accepted models currently are lm and glm.")
+         Accepted models currently are lm, glm, lmere, and glmer.")
 
   if(!(class(model)[1] %in% c("lm", "glm")))
     stop("Accepted models currently are lm and glm.")
@@ -38,6 +38,7 @@ resid_cookd <- function(model, theme, axis.text.size, title.text.size, title){
     geom_hline(yintercept = cutoff, colour = "blue", linetype = 5) +
     geom_hline(yintercept = 0, colour = "black")
 
+  if(!is.na(theme)){
   # Add theme to plot
   if (theme == "bw"){
     plot <- plot + theme_bw()
@@ -46,7 +47,7 @@ resid_cookd <- function(model, theme, axis.text.size, title.text.size, title){
   } else if (theme == "gray" | theme == "grey"){
     plot <- plot + theme_grey()
   }
-
+}
   # Set text size of title and axis lables, determine whether to include a title, and return plot
   if(title == TRUE){
     plot +
