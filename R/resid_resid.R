15a0724954c262fe08039b82f41ac8c1f8b05ec3
reykp <kathleenrey89@gmail.com>
2018-04-22 18:57:07 -0500

    working on standardized for lmer

--- R/resid_resid.R
+++ R/resid_resid.R
@@ -7,9 +7,42 @@
 # @return The appropriate residual label.
 
 resid_label <- function(type=NA, model){
-  if(!(is.na(type))){
-    if(!(type %in% c("response", "pearson", "deviance", "standardized", "stand.pearson", "stand.deviance")))
-      stop("The requested residual type is not available. Choose from the following options: standard, pearson, deviance.")
+
+
+  if(class(model)[1]=="lm"){
+    if(is.na(type)|type=="response"){
+      return(resid(model, type="response"))
+    }else if(type=="pearson"){
+      return(resid(model, type="response")/summary(model)$sigma)
+    }else(iftype=="standardized"){
+      return(stdres(model))
+    }
+  }
+
+
+  if (class(model)[1]=="glm"){
+    if(is.na(type)|type=="deviance"){
+      return(resid(model, type="deviance"))
+    }else if (type=="response"){
+      return(resid(model, type="response"))
+    }else if (type="pearson"){
+      return(resid(model, type="pearson"))
+    }else if (type=="stand.deviance"){
+      return((resid(model, type="deviance"))/(sqrt(summary(model)$dispersion*(1-hatvalues(model)))))
+    }else if (type=="stand.pearson"){
+      return((resid(model, type="pearson"))/(sqrt(summary(model)$dispersion*(1-hatvalues(model)))))
+    }
+  }
+
+  if (class(model)[1]=="lmerMod"){
+    if(is.na(type)|type="pearson"){
+      #condtional on blups
+      return(resid(model, type="response")/summary(model)$sigma)
+    }else if (type=="response"){
+      return(resid(model, type="response"))
+    }else if (type="standardized"){
+
+    }
   }
 
   if(!is.na(type)){
