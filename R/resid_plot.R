a2274b93ae834a0e99dfc515e1db89ea3f72977a
reykp <kathleenrey89@gmail.com>
2018-04-21 19:11:36 -0500

    add label to resid plot

--- R/resid_plot.R
+++ R/resid_plot.R
@@ -11,22 +11,32 @@
 # model <- lm(Volume ~ Girth, data = trees)
 # resid_plot(model)
 
-resid_plot <- function(model, smoother = FALSE, theme, axis.text.size, title.text.size, title){
+resid_plot <- function(model, type=NA, smoother = FALSE, theme="bw", axis.text.size=12, title.text.size=12, title=TRUE){
 
   # Return an error if a model is not entered in the function
   if(typeof(model) == "double")
     stop("The updated version of ggResidpanel requires a model to be input to the functions.
          Accepted models currently are lm and glm.")
 
+  r_label <- resid_label(type, model)
+  Default_Title <- paste(r_label,"Plot")
+  # Create a data frame with the residuals
+  if(is.na(type)){
+    model_values <- data.frame(resid = resid(model),
+                               pred = fitted(model))
+  }else{
+    model_values <- data.frame(resid = resid(model,type=type),
+                               pred = fitted(model))
+  }
+
   # Create a data frame with the residuals and predicted values
-  model_values <- data.frame(resid = resid(model),
-                             pred = fitted(model))
+
 
   # Create the residual plot
   plot <- ggplot(model_values, aes(x = pred, y = resid)) +
     geom_point() +
     geom_abline(slope = 0, intercept = 0, color = "blue") +
-    labs(x = "Predicted Values", y = "Residuals")
+    labs(x = "Predicted Values", y = r_label)
 
   # If smoother is set to true, add it to the plot
   if (smoother == TRUE){
@@ -46,7 +56,7 @@ resid_plot <- function(model, smoother = FALSE, theme, axis.text.size, title.tex
   # Set text size of title and axis lables, determine whether to include a title, and return plot
   if(title == TRUE){
     plot +
-      labs(title = "Residual Plot") +
+      labs(title = Default_Title) +
       theme(plot.title = element_text(size = title.text.size, face = "bold"),
             axis.title = element_text(size = axis.text.size))
   } else if (title == FALSE){
