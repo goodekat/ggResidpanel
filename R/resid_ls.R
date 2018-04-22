32a2f18152483f2c81f458227663b8c33a7c9e6d
reykp <kathleenrey89@gmail.com>
2018-04-21 19:00:12 -0500

    Labels

--- R/resid_ls.R
+++ R/resid_ls.R
@@ -9,26 +9,40 @@
 # model <- lm(Volume ~ Girth, data = trees)
 # resid_ls(model)
 
-resid_ls <- function(model, theme, axis.text.size, title.text.size, title){
+resid_ls <- function(model, type=NA,theme="bw", axis.text.size=12, title.text.size=12, title=TRUE){
 
   # Return an error if a model is not entered in the function
   if(typeof(model) == "double")
     stop("The updated version of ggResidpanel requires a model to be input to the functions.
-         Accepted models currently are lm and glm.")
+         Accepted models currently are lm, glm, lmer, and glmer.")
 
   if(!(class(model)[1] %in% c("lm", "glm")))
     stop("Accepted models currently are lm and glm.")
+
 
   # Create a data frame with the square root of the standardized residuals and predicted values
   model_values <- data.frame(sqr_stand_resid = sqrt(abs(rstandard(model))),
                              pred = fitted(model))
 
-  # Create the location-scale plot
-  plot <- ggplot(model_values, aes(x = pred, y = sqr_stand_resid)) +
-    geom_point() +
-    labs(x = "Predicted Values", y = expression(sqrt(abs("Standardized Residuals")))) +
-    expand_limits(y = 0) +
-    geom_smooth(colour = "red", se = FALSE, method = "loess", size = 0.5)
+
+  if (class(model)[1]=="lm"){
+    plot <- ggplot(model_values, aes(x = pred, y = sqr_stand_resid)) +
+      geom_point() +
+      labs(x = "Predicted Values", y = expression(sqrt(abs(" Standardized Residuals  ")))) +
+      expand_limits(y = 0) +
+      geom_smooth(colour = "red", se = FALSE, method = "loess", size = 0.5)
+
+  }else{
+    plot <- ggplot(model_values, aes(x = pred, y = sqr_stand_resid)) +
+      geom_point() +
+      labs(x = "Predicted Values", y = expression(sqrt(abs(" Standardized Deviance Residuals  ")))) +
+      expand_limits(y = 0) +
+      geom_smooth(colour = "red", se = FALSE, method = "loess", size = 0.5)
+
+  }
+
+    # Create the location-scale plot
+
 
   # Add theme to plot
   if (theme == "bw"){
