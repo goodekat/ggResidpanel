6f83d32f60a2087b4fbcbbb822ef28c9df40a1e7
goodekat <kgoode@iastate.edu>
2018-04-28 23:19:51 -0500

    cleaned up resid_respred

--- R/resid_respred.R
+++ R/resid_respred.R
@@ -1,50 +1,46 @@
 # Response versus predicted plot.
-#
-# Creates a plot of the response variable versus the predicted values.
 
+# Creates a plot of the response variable versus the predicted values
 resid_respred <- function(model, theme, axis.text.size, title.text.size, title.opt){
 
   ## Creation of Values to Plot -----------------------------------------------------
-  ## Creation of Labels -------------------------------------------------------------
-  ## Creation of Plot ---------------------------------------------------------------
-
-  #model_values$line <- model_values$pred
-  # Create the plot of response variable versus predicted values
-  Data <- resid_plotly_label(model)
 
   # Create a data frame with the predicted values and response variable
-
-  if(class(model)[1]=="glm"){
-    if(model$family[[1]]=="binomial"){
-    model_values <- data.frame(pred = fitted(model),
-                               response = resid_glm_actual(model))
-    }else{
-      model_values <- data.frame(pred = fitted(model),
-                                 response = model.frame(model)[[1]])
+  # based on the model type
+  if(class(model)[1] == "glm"){
+    if(model$family[[1]] == "binomial"){
+      model_values <- data.frame(Predicted = fitted(model),
+                                 Response = resid_glm_actual(model))
+    } else{
+      model_values <- data.frame(Predicted = fitted(model),
+                                 Response = model.frame(model)[[1]])
     }
-  }else if (class(model)[1]=="glmerMod"){
-    if(model@resp$family[[1]]=="binomial"){
-      model_values <- data.frame(pred = fitted(model),
-                                 response = resid_glm_actual(model))
-    }else{
-      model_values <- data.frame(pred = fitted(model),
-                                 response = model.frame(model)[[1]])
+  } else if (class(model)[1] == "glmerMod"){
+    if(model@resp$family[[1]] == "binomial"){
+      model_values <- data.frame(Predicted = fitted(model),
+                                 Response = resid_glm_actual(model))
+    } else{
+      model_values <- data.frame(Predicted = fitted(model),
+                                 Response = model.frame(model)[[1]])
     }
-  }else{
-    model_values <- data.frame(pred = fitted(model),
-                                   response = model.frame(model)[[1]])
+  } else{
+    model_values <- data.frame(Predicted = fitted(model),
+                               Response = model.frame(model)[[1]])
   }
 
-  plot <- ggplot(model_values, aes(x = pred, y = response,label=Data)) +
+  ## Creation of Labels -------------------------------------------------------------
+
+  # Create labels for plotly
+  Data <- resid_plotly_label(model)
+
+  ## Creation of Plot ---------------------------------------------------------------
+
+  # Create the plot of response variable versus predicted values
+  plot <- ggplot(model_values, aes(x = Predicted, y = Response, label = Data)) +
     geom_point() +
     geom_abline(slope = 1, intercept = 0, color = "blue") +
     labs(x = "Predicted Values", y = names(model.frame(model)[1]))
 
-
-  # plot <- ggplot(model_values, aes(x = pred, y = response)) +
-  #   geom_point() +
-  #   geom_line(aes(pred, line), color = "blue") +
-  #   labs(x = "Predicted Values", y = names(model.frame(model)[1]))
   # Add theme to plot
   if (theme == "bw"){
     plot <- plot + theme_bw()
@@ -54,7 +50,8 @@ resid_respred <- function(model, theme, axis.text.size, title.text.size, title.o
     plot <- plot + theme_grey()
   }
 
-  # Set text size of title and axis lables, determine whether to include a title, and return plot
+  # Set text size of title and axis lables, determine whether to include a title,
+  # and return plot
   if(title.opt == TRUE){
     plot +
       labs(title = "Response vs Predicted") +
