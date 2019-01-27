f9550bd62c8399f53fd726460f85d8d1c4cea96a
reykp <kathleenrey89@gmail.com>
2019-01-27 16:50:39 -0500

    Creating data from bimodial model more general

--- R/helper_plotly_label.R
+++ R/helper_plotly_label.R
@@ -23,43 +23,53 @@ if(class(model)[1]%in%c("lm", "glm")){
   if(class(model)[1]=="glm"){
     if(model$family[[1]]=="binomial"){
 
-      #The next set of code extracts the column names from the
-      #cbind statement used to create the number of successes and
-      #number of failures
+      ### OLD METHOD##################### Changed 1/27/19
 
-      #Find first parentheses
-      firstp <- as.numeric(gregexpr(pattern ='\\(',names_data[1])[1])
-      #Find end parentheses
-      lastp <- as.numeric(gregexpr(pattern ='\\)',names_data[1])[1])
-      #Find first comma
-      firstc <- as.numeric(gregexpr(pattern ='\\,',names_data[1])[1])
-      #Grab the name of the number of successes which is between the
-      #first parentheses and the comma
-      names(plotly_data)[1] <- str_sub(names_data[1], {firstp+1}, {firstc-1})
+      # #The next set of code extracts the column names from the
+      # #cbind statement used to create the number of successes and
+      # #number of failures
+      #
+      # #Find first parentheses
+      # firstp <- as.numeric(gregexpr(pattern ='\\(',names_data[1])[1])
+      # #Find end parentheses
+      # lastp <- as.numeric(gregexpr(pattern ='\\)',names_data[1])[1])
+      # #Find first comma
+      # firstc <- as.numeric(gregexpr(pattern ='\\,',names_data[1])[1])
+      # #Grab the name of the number of successes which is between the
+      # #first parentheses and the comma
+      # names(plotly_data)[1] <- str_sub(names_data[1], {firstp+1}, {firstc-1})
+      #
+      # #Check if the person gave the number of failures or calculated the number
+      # #of failures
+      # if (grepl("\\-", names_data[1])){
+      #   #Find the '-' sign: the name of the number of trials will preceed it
+      #   firstm <- as.numeric(gregexpr(pattern ='\\-',names_data[1])[1])
+      #   #Set the name of the trials by extracting everything from the comma
+      #   #to the minus sign and remove extra blanks
+      #   names(plotly_data)[2] <- gsub(" ", "",str_sub(names_data[1], {firstc+1}, {firstm-1}))
+      #
+      #   #Make sure are numeric (I no longer need these so commented out)
+      #   plotly_data[,1] <- as.numeric(as.character(plotly_data[,1]))
+      #   plotly_data[,2] <- as.numeric(as.character(plotly_data[,2]))
+      #
+      #   #Second column needs to contain total so add first two
+      #   plotly_data[,2] <- plotly_data[,1]+plotly_data[,2]
+      # }else{
+      #   #if the second column does not contain a minus, the user ented the number
+      #   #of failures so don't add the first two together and grab the name
+      #   #of the failures between the comma and the end parentheses and removed extra
+      #   #blanks
+      #   names(plotly_data)[2] <- gsub(" ", "",str_sub(names_data[1], {firstc+1}, {lastp-1}))
+      # }
 
-      #Check if the person gave the number of failures or calculated the number
-      #of failures
-      if (grepl("\\-", names_data[1])){
-        #Find the '-' sign: the name of the number of trials will preceed it
-        firstm <- as.numeric(gregexpr(pattern ='\\-',names_data[1])[1])
-        #Set the name of the trials by extracting everything from the comma
-        #to the minus sign and remove extra blanks
-        names(plotly_data)[2] <- gsub(" ", "",str_sub(names_data[1], {firstc+1}, {firstm-1}))
+      names(plotly_data)[1] <- "Success"
+      names(plotly_data)[2] <- "Total"
+      plotly_data$Success <- as.numeric(as.character(plotly_data$Success))
+      plotly_data$Total <- as.numeric(as.character(plotly_data$Total))
 
-        #Make sure are numeric (I no longer need these so commented out)
-        plotly_data[,1] <- as.numeric(as.character(plotly_data[,1]))
-        plotly_data[,2] <- as.numeric(as.character(plotly_data[,2]))
+      plotly_data$Total <- plotly_data$Total+plotly_data$Success
 
-        #Second column needs to contain total so add first two
-        plotly_data[,2] <- plotly_data[,1]+plotly_data[,2]
-      }else{
-        #if the second column does not contain a minus, the user ented the number
-        #of failures so don't add the first two together and grab the name
-        #of the failures between the comma and the end parentheses and removed extra
-        #blanks
-        names(plotly_data)[2] <- gsub(" ", "",str_sub(names_data[1], {firstc+1}, {lastp-1}))
       }
-    }
   }
   #Create a variable containing the observation number
   plotly_data$Obs <- 1:nrow(plotly_data)
@@ -107,32 +117,42 @@ if(class(model)[1]%in%c("lm", "glm")){
   if(class(model)[1]=="glmerMod"){
     if(model@resp$family[[1]]=="binomial"){
 
-      #Find first parentheses
-      firstp <- as.numeric(gregexpr(pattern ='\\(',names_data[1])[1])
-      #Find end parentheses
-      lastp <- as.numeric(gregexpr(pattern ='\\)',names_data[1])[1])
-      #Find first comma
-      firstc <- as.numeric(gregexpr(pattern ='\\,',names_data[1])[1])
-      #Grab the name of the number of successes
-      names(plotly_data)[1] <- str_sub(names_data[1], {firstp+1}, {firstc-1})
+      #### OLD VERSION ##################### Changed 1/27/2019
 
-      #Check if the person gave the number of failures or calculated the number
-      #of failures
-      if (grepl("\\-", names_data[1])){
-        #First the '-' sign
-        firstm <- as.numeric(gregexpr(pattern ='\\-',names_data[1])[1])
-        #Set the name of the total
-        names(plotly_data)[2] <- gsub(" ", "",str_sub(names_data[1], {firstc+1}, {firstm-1}))
+      # #Find first parentheses
+      # firstp <- as.numeric(gregexpr(pattern ='\\(',names_data[1])[1])
+      # #Find end parentheses
+      # lastp <- as.numeric(gregexpr(pattern ='\\)',names_data[1])[1])
+      # #Find first comma
+      # firstc <- as.numeric(gregexpr(pattern ='\\,',names_data[1])[1])
+      # #Grab the name of the number of successes
+      # names(plotly_data)[1] <- str_sub(names_data[1], {firstp+1}, {firstc-1})
+      #
+      # #Check if the person gave the number of failures or calculated the number
+      # #of failures
+      # if (grepl("\\-", names_data[1])){
+      #   #First the '-' sign
+      #   firstm <- as.numeric(gregexpr(pattern ='\\-',names_data[1])[1])
+      #   #Set the name of the total
+      #   names(plotly_data)[2] <- gsub(" ", "",str_sub(names_data[1], {firstc+1}, {firstm-1}))
+      #
+      #   #Make sure are numeric (don't need these lines anymore)
+      #   plotly_data[,1] <- as.numeric(as.character(plotly_data[,1]))
+      #   plotly_data[,2] <- as.numeric(as.character(plotly_data[,2]))
+      #
+      #   #Second column needs to contain total so add first two
+      #   plotly_data[,2] <- plotly_data[,1]+plotly_data[,2]
+      # }else{
+      #   names(plotly_data)[2] <- gsub(" ", "",str_sub(names_data[1], {firstc+1}, {lastp-1}))
+      # }
 
-        #Make sure are numeric (don't need these lines anymore)
-        plotly_data[,1] <- as.numeric(as.character(plotly_data[,1]))
-        plotly_data[,2] <- as.numeric(as.character(plotly_data[,2]))
+      names(plotly_data)[1] <- "Success"
+      names(plotly_data)[2] <- "Total"
+      plotly_data$Success <- as.numeric(as.character(plotly_data$Success))
+      plotly_data$Total <- as.numeric(as.character(plotly_data$Total))
 
-        #Second column needs to contain total so add first two
-        plotly_data[,2] <- plotly_data[,1]+plotly_data[,2]
-      }else{
-        names(plotly_data)[2] <- gsub(" ", "",str_sub(names_data[1], {firstc+1}, {lastp-1}))
-      }
+      plotly_data$Total <- plotly_data$Total+plotly_data$Success
+
     }
   }
   plotly_data$Obs <- 1:nrow(plotly_data)
